4e9134e0f5b4a75cc723efcd3338799d99bfa2bd
danwang112233 <wmlvbm613@gmail.com>
2017-09-15 16:16:57 +0200

    Add files via upload

---
+++ 03-md-phases-100/README.md
@@ -0,0 +1,6 @@
+
+
+Obtain phase transition temperatures. Lattice paramters are allowed to vary.
+using the Isotropic-anharmonic Shell Model proposed by Vielma and
+Schneider based on PBE Generalized Gradient Approximation(GGA) to 
+Density Functional Theory(DFT): [Vielma Schneider 2013](http://dx.doi.org/10.1063/1.4827475)
---
+++ 03-md-phases-100/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#-------------------------------------------------LATTICE PARAMETER a
+a="4.010"
+
+#-------------------------------------------------LATTICE PARAMETER alpha
+alpha="89.81"
+
+#-------------------------------------------------REPLICAS IN EACH DIRECTION
+nx="10"
+ny="10"
+nz="100"
+
+#-------------------------------------------------GENERATE DATA FILE
+python molsetup.py $a $nx $ny $nz
+
+#-------------------------------------------------SPONTANEOUS POLARIZATION
+# 0-lmpsetup.py 
+# Write Polarization Vs Temperature to file after  Molecular Static 
+# Simulation of Rhombohedral Barium Titanate at 0K with an electric field 
+# applied in [1 1 1] direction.
+
+mpirun -np 8 python 0-lmpsetup.py $a $alpha $nx $ny $nz
+
+#-------------------------------------------------GNUPLOT
+# Plot polarization 
+gnuplot plot.gnu
+
